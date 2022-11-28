c7f5f58da6deccd1e68a33dfe635b7dd569e2638
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-11-28 19:34:12 +0200

    Update RotationHelper.js

--- RotationHelper.js
+++ RotationHelper.js
@@ -53,7 +53,7 @@ function SetInitRotation(e)
             
             RotHelper.Initial_cmpss_val = cmpss180;
             
-           // RotHelper.CmpssEventCounter=0;
+           RotHelper.CmpssEventCounter=0;
             
             //CameraWrapper.camera.updateMatrix();
   
